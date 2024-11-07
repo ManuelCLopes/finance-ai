185f1816f8911c461adaab1c26641eedb02a2a50
ManuelCLopes <manuel_clopes@outlook.com>
2024-11-07 01:11:02 +0000

    feat: ensure transaction can be created

--- app/_components/add-transaction-button.tsx
+++ app/_components/add-transaction-button.tsx
@@ -44,13 +44,15 @@ import {
   TRANSACTION_TYPE_OPTIONS,
 } from "../_constants/transactions";
 import { DatePicker } from "./ui/date-picker";
+import { addTransaction } from "../_actions/add-transaction/index";
+import { useState } from "react";
 
 const formSchema = z.object({
   name: z.string().trim().min(1, {
     message: "O nome é obrigatório.",
   }),
-  amount: z.string().trim().min(1, {
-    message: "O nome é obrigatório.",
+  amount: z.number({
+    required_error: "O valor é obrigatório.",
   }),
   type: z.nativeEnum(TransactionType, {
     required_error: "O tipo é obrigatório.",
@@ -69,10 +71,11 @@ const formSchema = z.object({
 type FormSchema = z.infer<typeof formSchema>;
 
 const AddTransactionButton = () => {
+  const [dialogIsOpen, setDialogIsOpen] = useState(false);
   const form = useForm<FormSchema>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      amount: "",
+      amount: 0,
       category: TransactionCategory.OTHER,
       date: new Date(),
       name: "",
@@ -81,13 +84,21 @@ const AddTransactionButton = () => {
     },
   });
 
-  const onSubmit = (data: FormSchema) => {
-    console.log(data);
+  const onSubmit = async (data: FormSchema) => {
+    try {
+      await addTransaction(data);
+      setDialogIsOpen(false);
+      form.reset();
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <Dialog
+      open={dialogIsOpen}
       onOpenChange={(open) => {
+        setDialogIsOpen(open);
         if (!open) {
           form.reset();
         }
@@ -127,7 +138,15 @@ const AddTransactionButton = () => {
                 <FormItem>
                   <FormLabel>Valor</FormLabel>
                   <FormControl>
-                    <MoneyInput placeholder="Digite o valor..." {...field} />
+                    <MoneyInput
+                      placeholder="Digite o valor..."
+                      onValueChange={({ floatValue }) => {
+                        field.onChange(floatValue);
+                      }}
+                      value={field.value}
+                      onBlur={field.onBlur}
+                      disabled={field.disabled}
+                    />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
--- app/_constants/transactions.ts
+++ app/_constants/transactions.ts
@@ -43,35 +43,35 @@ export const TRANSACTION_TYPE_OPTIONS = [
 
 export const TRANSACTION_PAYMENT_METHOD_OPTIONS = [
   {
-    value: TRANSACTION_PAYMENT_METHOD_LABELS.BANK_TRANSFER,
+    value: TransactionPaymentMethod.BANK_TRANSFER,
     label:
       TRANSACTION_PAYMENT_METHOD_LABELS[TransactionPaymentMethod.BANK_TRANSFER],
   },
   {
-    value: TRANSACTION_PAYMENT_METHOD_LABELS.BANK_SLIP,
+    value: TransactionPaymentMethod.BANK_SLIP,
     label:
       TRANSACTION_PAYMENT_METHOD_LABELS[TransactionPaymentMethod.BANK_SLIP],
   },
   {
-    value: TRANSACTION_PAYMENT_METHOD_LABELS.CASH,
+    value: TransactionPaymentMethod.CASH,
     label: TRANSACTION_PAYMENT_METHOD_LABELS[TransactionPaymentMethod.CASH],
   },
   {
-    value: TRANSACTION_PAYMENT_METHOD_LABELS.CREDIT_CARD,
+    value: TransactionPaymentMethod.CREDIT_CARD,
     label:
       TRANSACTION_PAYMENT_METHOD_LABELS[TransactionPaymentMethod.CREDIT_CARD],
   },
   {
-    value: TRANSACTION_PAYMENT_METHOD_LABELS.DEBIT_CARD,
+    value: TransactionPaymentMethod.DEBIT_CARD,
     label:
       TRANSACTION_PAYMENT_METHOD_LABELS[TransactionPaymentMethod.DEBIT_CARD],
   },
   {
-    value: TRANSACTION_PAYMENT_METHOD_LABELS.OTHER,
+    value: TransactionPaymentMethod.OTHER,
     label: TRANSACTION_PAYMENT_METHOD_LABELS[TransactionPaymentMethod.OTHER],
   },
   {
-    value: TRANSACTION_PAYMENT_METHOD_LABELS.PIX,
+    value: TransactionPaymentMethod.PIX,
     label: TRANSACTION_PAYMENT_METHOD_LABELS[TransactionPaymentMethod.PIX],
   },
 ];
